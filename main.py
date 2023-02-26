509adbed1cd34df40ddf90e8cc3d34d586259d52
Marco Saufaus <Marco.Saufaus2@gmail.com>
2023-02-26 18:09:44 +0100

    Setup game window

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import pygame as pg
 import random
 from settings import *
 
-
+# Initial Setup
 pg.init()
 screen = pg.display.set_mode([WIDTH, HEIGHT])
 pg.display.set_caption("2048")
 timer = pg.time.Clock()
 font = pg.font.Font("freesansbold.ttf", 24)
 
+# Main Game Loop
+run = True
+while run:
+    timer.tick(FPS)
+    screen.fill("gray")
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+    pg.display.flip()
